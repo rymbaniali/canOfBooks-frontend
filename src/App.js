c7aeab52bfa917ab1c2dae9db979122e10a27fd1
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-20 15:40:10 +0300

    add logut and user as driver

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import Login from './components/login'
+import React, { Component } from 'react';
+import Login from './components/login';
+import  Logout from './components/logout';
+import User from './components/user';
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Login/>
+        <Logout/>
+        <User/>
       </div>
     )
   }
 }
 
-export default App
+export default App;
