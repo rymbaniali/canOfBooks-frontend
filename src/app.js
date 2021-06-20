25d60c5571b03376f7abb1c9eccab525d566bdfc
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-20 17:01:41 +0300

    added component as driver

--- src/app.js
+++ src/app.js
@@ -1,28 +1,48 @@
 import React from 'react';
-import Header from './Header';
+import Header from './header';
 import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
+import Footer from './footer';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/profile';
+import MyFavoriteBooks from './components/MyFavouritBooks'
+import { withAuth0 } from "@auth0/auth0-react";
 
 class App extends React.Component {
 
   render() {
+    const { isAuthenticated } = this.props.auth0;
     console.log('app', this.props)
-    return(
+    return (
       <>
         <Router>
           <IsLoadingAndError>
             <Header />
-              <Switch>
-                <Route exact path="/">
-                  {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
-                </Route>
-                {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-              </Switch>
+            <Switch>
+              <Route exact path="/">
+
+                {isAuthenticated ? <MyFavoriteBooks/>:<LoginButton/>
+                
+                /* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+              </Route>
+             
+              {isAuthenticated && (
+                <>
+                  <Profile />
+                </>
+              )}
+
+              { /* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+
+
+              <LoginButton />
+              <LogoutButton />
+            </Switch>
             <Footer />
           </IsLoadingAndError>
         </Router>
@@ -31,4 +51,5 @@ class App extends React.Component {
   }
 }
 
-export default App;
+
+export default withAuth0(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
